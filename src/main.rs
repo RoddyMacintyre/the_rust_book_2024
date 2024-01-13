a3ef31377e28a8affd1983b7e7725080f4b552dc
RoddyWiertsema <itsdigits@gmail.com>
2024-01-13 15:49:43 +0100

    Compile basic program.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// In Rust main func is the entry point in every executable
+
 fn main() {
+    // Indent with 4 spaces, not a tab
+    // Println! is a macro, recognizable by the !
     println!("Hello, world!");
 }
