6f720b9b090d33e6ebe64d4cca2627f11d8cb646
RoddyWiertsema <itsdigits@gmail.com>
2024-01-19 17:42:01 +0100

    Place bulk of main in a forever loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,26 +10,29 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);  // By default an i32
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess...");
+    loop {
+        println!("Please input your guess...");
 
-    // Declare var to store input in
-    let mut guess = String::new();
-    // Get the input and place it in the var
-    io::stdin()
-        .read_line(&mut guess)// Need to make the reference mutable as well, hence `&mut guess`
-        .expect("Failed to read line!");    // Catch the Err variant of the Result Enum, or return the Ok value (n bytes of the user input)
 
-    // Cast guess to an unsigned 32 bit int
-    let guess: u32 = guess.trim().parse().expect("Not a number, please type a number!");
+        // Declare var to store input in
+        let mut guess = String::new();
+        // Get the input and place it in the var
+        io::stdin()
+            .read_line(&mut guess)// Need to make the reference mutable as well, hence `&mut guess`
+            .expect("Failed to read line!");    // Catch the Err variant of the Result Enum, or return the Ok value (n bytes of the user input)
 
-    println!("You guessed: {guess}");
+        // Cast guess to an unsigned 32 bit int. Parse() is the part converting from string to another type.
+        let guess: u32 = guess.trim().parse().expect("Not a number, please type a number!");
 
-    // Ordering is a type with Enums `Less`, `Greater` & `Equal`. guess is compared to secret_number here
-    // Match checks which output cme from the compare, and executes on the match.
-    // Matches have arms that rep[resent a pattern to match against, and the code that should be run if matched
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => prinln!("You win!"),
+        println!("You guessed: {guess}");
+
+        // Ordering is a type with Enums `Less`, `Greater` & `Equal`. guess is compared to secret_number here
+        // Match checks which output cme from the compare, and executes on the match.
+        // Matches have arms that rep[resent a pattern to match against, and the code that should be run if matched
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
