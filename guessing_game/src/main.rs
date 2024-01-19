e8943eda53f541059c9e664b10c03d6569c7272d
RoddyWiertsema <itsdigits@gmail.com>
2024-01-19 17:34:23 +0100

    Add pattern matching of guess & secret number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -6,7 +7,7 @@ fn main() {
 
     // Generate random number with a generator local to the current thread and is seeded by the OS
     // The range is specified as 1..=100 which means 1-100 inclusive
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = rand::thread_rng().gen_range(1..=100);  // By default an i32
     println!("The secret number is: {secret_number}");
 
     println!("Please input your guess...");
@@ -19,4 +20,13 @@ fn main() {
         .expect("Failed to read line!");    // Catch the Err variant of the Result Enum, or return the Ok value (n bytes of the user input)
 
     println!("You guessed: {guess}");
+
+    // Ordering is a type with Enums `Less`, `Greater` & `Equal`. guess is compared to secret_number here
+    // Match checks which output cme from the compare, and executes on the match.
+    // Matches have arms that rep[resent a pattern to match against, and the code that should be run if matched
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => prinln!("You win!"),
+    }
 }
