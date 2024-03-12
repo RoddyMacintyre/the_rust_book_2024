6486689f435dda70a3d96bac859c26329ea6db6d
RoddyWiertsema <itsdigits@gmail.com>
2024-03-12 22:50:03 +0100

    The slice type: String slices

--- the_slice_type/src/main.rs
+++ the_slice_type/src/main.rs
@@ -25,6 +25,24 @@ fn first_word(s: &String) -> usize {
     s.len()
 }
 
+// ========== STRING SLICES ==========
+/*
+Reference to a part of a string
+String slices become str types
+
+Slices are "fat" pointers: pointers with metadata
+(pointer and len which define the underlined regions of the string on the heap)
+
+Slices are references, so they change the permissions on referenced data
+The original String loses write and own permissions when a slice is made and regains it after creation
+
+let s = String::from("hello world");
+
+let hello: &str = &s[0..5];     // hello
+let world: &str = &s[6..11];    // world
+let s2: &String = &s;
+ */
+
 fn main() {
     let mut s = String::from("Roddy Macintyre");    // s on the stack, "Roddy Macintyre" on the heap
     let index_found = first_word(&s);
